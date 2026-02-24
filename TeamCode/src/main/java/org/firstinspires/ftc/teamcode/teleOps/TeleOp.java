9cfe722f39a25204faecfe32cb31bae72662e6f1
jayjay <167156089+jjnootfound@users.noreply.github.com>
2026-02-23 19:10:17 -0600

    feat: update teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOps/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOps/TeleOp.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.subsystems.TanquesitoBonito;
 
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
 public class TeleOp extends LinearOpMode {
     private TanquesitoBonito tanquesitoBonito;
     @Override
